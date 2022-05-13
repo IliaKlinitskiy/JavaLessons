ef387e7fc10dccf958a80825eba81e5d47ccd804
Ilia Klinitskiy <ilyaklinitskiy2000@gmail.com>
2022-05-13 17:10:02 +0300

    oop homework - constructor

---
+++ src/homeWorkConstructor/Main.java
@@ -0,0 +1,9 @@
+package homeWorkDesigner;
+
+public class Main {
+    public static void main(String[] args) {
+        Worker worker = new Worker("Вася", "Мэр", 120000);
+        System.out.println(worker.getInfo(12));
+    }
+
+}
---
+++ src/homeWorkConstructor/Worker.java
@@ -0,0 +1,16 @@
+package homeWorkDesigner;
+
+public class Worker {
+    String name;
+    String post;
+    int salary;
+
+    public Worker(String name, String post, int salary) {
+        this.name = name;
+        this.post = post;
+        this.salary = salary;
+    }
+    String getInfo (int month) {
+        return "Имя - " + name + ". Должность - " + post + ". За посдение " + month + " месяцев заработал " + salary * month + " рублей.";
+    }
+}
