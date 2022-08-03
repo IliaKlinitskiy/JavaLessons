4331373fdc2bf58f105406f5e8e0140d73755c2c
lialinitskiy <ilyaklinitskiy2000@gmail.com>
2022-08-03 19:25:44 +0300

    lesson - collections2

--- src/lessonCollection/Test.java
+++ src/lessonCollection/Test.java
@@ -2,15 +2,6 @@ package lessonCollection;
 
 public class Test {
     public static void main(String[] args) {
-        MyArrayTest gpa = new MyArrayTest();
-        gpa.add("Роман");
-        gpa.add("Сергей");
-        gpa.add("Марк");
-        gpa.add("Тимоха");
-        gpa.add("Илья");
-        gpa.remove("Тимоха");
-        for (int i = 0; i < gpa.getSize(); i++) {
-            System.out.println(gpa.get(i));
-        }
+
     }
 }
