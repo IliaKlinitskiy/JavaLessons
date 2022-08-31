afe9d5fa70f6596ebd31c5d8fbfc8614ac04c985
Ilya Klinitskiy <ilyaklinitskiy2000@gmail.com>
2022-08-31 17:19:52 +0300

    lesson - ArrayList

---
+++ src/arrayList/CarList.java
@@ -0,0 +1,12 @@
+package arrayList;
+
+public interface CarList {
+    Car get(int index);
+    void add (Car car);
+    void add (Car car, int index);
+    boolean remove(Car car);
+    boolean removeAt(int index);
+    int size();
+    void clear();
+
+}
