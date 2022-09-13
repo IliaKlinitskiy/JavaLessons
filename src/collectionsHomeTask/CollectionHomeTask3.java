7a76481ea851c3dc805121a460543837662a78d2
lialinitskiy <ilyaklinitskiy2000@gmail.com>
2022-09-13 14:06:30 +0300

    collectionHomeTask - 3

---
+++ src/collectionsHomeTask/CollectionHomeTask3.java
@@ -0,0 +1,11 @@
+package collectionsHomeTask;
+
+public class CollectionHomeTask3 {
+    public static void main(String[] args) {
+        String str = "Hello world!";
+        String b = str.substring(0, str.length() - 7);
+        System.out.println(b);
+        String a = str.substring(6, str.length() - 1);
+        System.out.println(a);
+    }
+}
